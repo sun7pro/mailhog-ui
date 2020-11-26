226639fe545c7c1f243c9e77938a7a9e2dfefc72
Tuan Pham <tuanpht.online@gmail.com>
2020-11-27 04:12:17 +0700

    Using react-window to render list

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import './App.css';
 import EmailContent from './EmailContent';
 import EmailSummary from './EmailSummary';
 import { HashRouter as Router, Switch, Route, useParams } from 'react-router-dom';
+import { FixedSizeList as List } from 'react-window';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import InfiniteLoader from 'react-window-infinite-loader';
+
+const PER_PAGE = 5;
 
 function EmailApp() {
   const [data, setData] = useState({});
   const [currentEmail, setCurrentEmail] = useState(null);
+  const [dataStartIndex, setDataStartIndex] = useState(0);
 
   useEffect(() => {
-    fetch('/api/v2/messages?limit=2')
+    fetch(`/api/v2/messages?limit=${PER_PAGE}&start=${dataStartIndex}`)
       .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+      .then((data) =>
+        setData((prevData) => {
+          if (!prevData.items) {
+            return data;
+          }
+          const newData = { ...prevData };
+          newData.items = newData.items.concat(data.items);
+          return newData;
+        })
+      );
+  }, [dataStartIndex]);
 
   const onEmailClick = (email) => () => {
     setCurrentEmail(email);
@@ -38,14 +53,41 @@ function EmailApp() {
         </a>
       </header>
       <aside>
-        <ul>
-          {data.total > 0 &&
-            data.items.map((email) => (
-              <li key={email.ID}>
-                <EmailSummary email={email} onEmailClick={onEmailClick} />
-              </li>
-            ))}
-        </ul>
+        {data.total > 0 && (
+          <AutoSizer>
+            {({ width, height }) => (
+              <InfiniteLoader
+                itemCount={data.total}
+                isItemLoaded={(index) => {
+                  return !!data.items[index];
+                }}
+                loadMoreItems={(startIndex) => {
+                  setDataStartIndex(startIndex);
+                }}
+              >
+                {({ onItemsRendered, ref }) => (
+                  <List
+                    onItemsRendered={onItemsRendered}
+                    ref={ref}
+                    height={height}
+                    itemData={data.items}
+                    itemCount={data.total}
+                    itemSize={50}
+                    width={width}
+                  >
+                    {({ index, style, data: items }) => {
+                      return (
+                        <div style={style}>
+                          {items[index] && <EmailSummary email={items[index]} onEmailClick={onEmailClick} />}
+                        </div>
+                      );
+                    }}
+                  </List>
+                )}
+              </InfiniteLoader>
+            )}
+          </AutoSizer>
+        )}
       </aside>
       <main className="content">
         <EmailContent email={currentEmail} />
@@ -57,7 +99,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/:emailId">
+        <Route exact path="/:emailId?">
           <EmailApp />
         </Route>
       </Switch>
--- src/EmailSummary.js
+++ src/EmailSummary.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function EmailSummary({ email, onEmailClick }) {
   return (
-    <Link to={'/' + email.ID} onClick={onEmailClick(email)}>
+    <Link className="email-summary" to={'/' + email.ID} onClick={onEmailClick(email)}>
       <span>{email.Content.Headers.Subject[0]}</span>
       <br />
       <span>{email.Content.Headers.To[0]}</span>
